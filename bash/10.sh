0c8b872b12a57736a2ce576523412296ac4b247c
jeroen-manders <jeroen.manders@external.mckinsey.com>
2020-08-02 14:27:05 +0200

    Fix file version

--- bash/10.sh
+++ bash/10.sh
@@ -2,5 +2,4 @@
 
 set -eo pipefail;
 
-# This currently gives "Permission denied"
-# docker pull quay.io/jeroenmanders/infraxys-provisioning-server:ubuntu-full-18.04-latest;
+docker pull quay.io/jeroenmanders/infraxys-provisioning-server:ubuntu-full-18.04-latest;
